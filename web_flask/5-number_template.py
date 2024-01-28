0293f71d8e57737b74080e9c2f8c551b0392af66
Ngajimbajr <ngajimbajr@gmail.com>
2024-01-28 04:13:01 -0800

    web_flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 4. Add fourth view function that displays var only if is integer """
+""" 5. Add fifth view func that displays HTML page if n is int """
 
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -40,6 +41,13 @@ def number_text(n):
     """ replace with int only if given int. """
     n = str(n)
     return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
